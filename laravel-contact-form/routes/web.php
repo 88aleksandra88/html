455725d1ef77d6753e8ed31c07c04b4731038b5f
aleks <aleksandra.slowik88@gmail.com>
2020-11-04 15:42:49 +0100

    add css

--- laravel-contact-form/routes/web.php
+++ laravel-contact-form/routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Render in view
+Route::get('/contact', [
+    'uses' => 'ContactUsFormController@createForm'
+]);
+
+// Post form data
+Route::post('/contact', [
+    'uses' => 'ContactUsFormController@ContactUsForm',
+    'as' => 'contact.store'
+]);
