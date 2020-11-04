cc6a3d81ee642db0898473ce7e56b9178be74962
aleks <aleksandra.slowik88@gmail.com>
2020-11-04 16:51:45 +0100

    .env uodate

--- contact-form/routes/web.php
+++ contact-form/routes/web.php
@@ -13,17 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Render in view
-Route::get('/contact', [
-    'uses' => 'ContactUsFormController@createForm'
-]);
-
-// Post form data
-Route::post('/contact', [
-    'uses' => 'ContactUsFormController@ContactUsForm',
-    'as' => 'contact.store'
-]);
+Route::get('/', function () {
+    return view('welcome');
+});
