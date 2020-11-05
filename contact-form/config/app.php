5a38487f28d4b5efe318264e6cabc22895dad09f
aleks <aleksandra.slowik88@gmail.com>
2020-11-05 21:18:30 +0100

    debug heroku

--- contact-form/config/app.php
+++ contact-form/config/app.php
@@ -226,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
+        'debug' => env('APP_DEBUG', true),
 
     ],
 
